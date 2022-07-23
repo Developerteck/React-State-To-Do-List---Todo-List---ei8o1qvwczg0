bddb04031eb61be41056d04e72ab357217762abb
Developerteck <66458929+Developerteck@users.noreply.github.com>
2022-07-23 19:14:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import "./../styles/App.css";
 
-function App() 
-{
+function App() {
+	const [input, setInput] = useState("");
+	const [list, setList]= useState([]);
+	const [selected, setSelected] = useState({})
+
 	return (
 	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
+		<textarea id="task" value={input} onChange={(e)=>{setInput(e.target.value)}} />
+		<button id="btn" onClick={()=>{if(!input) return ; setList(prev=>([...prev,input]));setInput("")}}>Add</button>
+		<ul>
+			{list.map((ele,index)=>{
+				return <li className="list">{ele} <button className="edit" onClick={()=>{setSelected({value:ele,index:index})}}>edit</button><button className="delete" onClick={()=>{setList(prev=>{
+					prev = prev.filter((ele,ind)=>{
+						return ind !== index
+					})
+					return prev;
+				})}}>delete</button></li>
+			})}
+		</ul>
+		{selected && selected.index >= 0 && <>
+			<textarea className="editTask" value={selected.value} onChange={(e)=>{setSelected({...selected,value:e.target.value})}}/>
+			<button className="saveTask" disabled={!selected.value} onClick={()=>{
+				setList(prev=>{prev[selected.index] = selected.value; return prev});
+				setSelected({})
+			}}>save</button>
+		</>}
 	</div>
 	);
 }
